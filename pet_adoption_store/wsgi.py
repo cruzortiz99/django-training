f715bb695f0f2d925072fd3e2e90f1607f4b3bb6
CruzOrtiz <cruzortiz099@gmail.com>
2022-12-11 09:08:50 -0400

    Starting pet adoption store project

--- pet_adoption_store/wsgi.py
+++ pet_adoption_store/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for training project.
+WSGI config for pet_adoption_store project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'training.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pet_adoption_store.settings')
 
 application = get_wsgi_application()
